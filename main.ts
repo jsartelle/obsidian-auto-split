c51e89b0aab9fd8392ffaa898f476d14bcdfd89b
James Sartelle <james.sartelle@moodmedia.com>
2021-10-28 23:05:26 -0400

    1.0.0
    Initial release

--- main.ts
+++ main.ts
@@ -1,112 +1,256 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
 
-interface MyPluginSettings {
-	mySetting: string;
+type SplitDirectionSetting = "vertical" | "horizontal" | "auto";
+type PaneTypeSetting = "source" | "preview";
+
+interface AutoSplitSettings {
+    minSize: number;
+    direction: SplitDirectionSetting;
+    editorFirst: boolean;
+    paneToFocus: PaneTypeSetting;
+    linkPanes: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: AutoSplitSettings = {
+    minSize: 1000,
+    direction: "auto",
+    editorFirst: true,
+    paneToFocus: "source",
+    linkPanes: true
+};
+
+export default class AutoSplitPlugin extends Plugin {
+    settings: AutoSplitSettings;
+
+    protected hasOpenFiles: boolean;
+    protected updateHasOpenFiles() {
+        try {
+            this.hasOpenFiles =
+                this.app.workspace.getLeavesOfType("markdown").length > 0;
+        } catch (e) {
+            // it's okay to fail sometimes
+        }
+    }
+
+    async onload() {
+        await this.loadSettings();
+
+        this.app.workspace.onLayoutReady(() => {
+            this.updateHasOpenFiles();
+
+            this.registerEvent(
+                this.app.workspace.on("file-open", async file => {
+                    if (!this.hasOpenFiles && file) {
+                        const newState = Object.assign(
+                            {},
+                            this.app.workspace.activeLeaf.getViewState()
+                        );
+
+                        newState.state.mode =
+                            newState.state.mode === "source"
+                                ? "preview"
+                                : "source";
+
+                        if (this.settings.linkPanes) {
+                            newState.group = this.app.workspace.activeLeaf;
+                        }
+
+                        const rootSize = getRootContainerSize(this.app);
+                        let direction = this.settings.direction;
+                        if (direction === "auto") {
+                            direction =
+                                rootSize.width >= rootSize.height
+                                    ? "vertical"
+                                    : "horizontal";
+                        }
+
+                        if (
+                            (direction === "vertical"
+                                ? rootSize.width
+                                : rootSize.height) > this.settings.minSize
+                        ) {
+                            const currentLeaf = this.app.workspace.activeLeaf;
+
+                            const viewState = currentLeaf.getViewState();
+                            viewState.state.mode =
+                                viewState.state.mode === "preview"
+                                    ? "source"
+                                    : "preview";
+
+                            const firstPane = this.settings.editorFirst
+                                ? "source"
+                                : "preview";
+
+                            const newLeaf =
+                                this.app.workspace.createLeafBySplit(
+                                    currentLeaf,
+                                    direction,
+                                    viewState.state.mode === firstPane
+                                );
+                            newLeaf.openFile(file, viewState);
+
+                            if (this.settings.linkPanes) {
+                                currentLeaf.setGroupMember(newLeaf);
+                            }
+
+                            if (
+                                viewState.state.mode ===
+                                this.settings.paneToFocus
+                            ) {
+                                this.app.workspace.setActiveLeaf(
+                                    newLeaf,
+                                    true,
+                                    true
+                                );
+                            }
+                        }
+                    }
+
+                    this.app.workspace.onLayoutReady(() => {
+                        this.updateHasOpenFiles();
+                    });
+                })
+            );
+        });
+
+        this.addSettingTab(new AutoSplitSettingTab(this.app, this));
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData()
+        );
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class AutoSplitSettingTab extends PluginSettingTab {
+    plugin: AutoSplitPlugin;
 
-	async onload() {
-		console.log('loading plugin');
+    constructor(app: App, plugin: AutoSplitPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-		await this.loadSettings();
+    display(): void {
+        let { containerEl } = this;
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+        containerEl.empty();
 
-		this.addStatusBarItem().setText('Status Bar Text');
+        containerEl.createEl("h2", { text: "Auto Split Settings" });
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+        const { width: rootWidth, height: rootHeight } = getRootContainerSize(
+            this.app
+        );
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+        new Setting(containerEl)
+            .setName("Minimum Main Area Size")
+            .setDesc(
+                `Only split if the main area is at least this wide or tall, depending on split direction. The main area was ${rootWidth}x${rootHeight} when you opened this tab. (default: 1000)`
+            )
+            .addText(text => {
+                text.inputEl.type = "number";
+                text.setValue(String(this.plugin.settings.minSize)).onChange(
+                    async value => {
+                        const valueAsNumber = Number.parseInt(value);
+                        this.plugin.settings.minSize = Number.isInteger(
+                            valueAsNumber
+                        )
+                            ? valueAsNumber
+                            : this.plugin.settings.minSize;
+                        await this.plugin.saveSettings();
+                    }
+                );
+            });
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
+        new Setting(containerEl)
+            .setName("Split Direction")
+            .setDesc(
+                "Vertical = left/right, Horizontal = up/down. Auto chooses based on the longer side of the main area."
+            )
+            .addDropdown(dropdown => {
+                dropdown
+                    .addOptions({
+                        vertical: "Vertical",
+                        horizontal: "Horizontal",
+                        auto: "Auto"
+                    })
+                    .setValue(this.plugin.settings.direction)
+                    .onChange(async value => {
+                        this.plugin.settings.direction =
+                            value as SplitDirectionSetting;
+                        await this.plugin.saveSettings();
+                    });
+            });
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+        new Setting(containerEl)
+            .setName("Editor First")
+            .setDesc(
+                "Place the editor pane on the left in vertical splits, or the top in horizontal splits."
+            )
+            .addToggle(toggle => {
+                toggle
+                    .setValue(this.plugin.settings.editorFirst)
+                    .onChange(async value => {
+                        this.plugin.settings.editorFirst = value;
+                        await this.plugin.saveSettings();
+                    });
+            });
 
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        new Setting(containerEl)
+            .setName("Pane To Focus")
+            .setDesc("Select which pane should be focused.")
+            .addDropdown(dropdown => {
+                dropdown
+                    .addOptions({
+                        source: "Editor",
+                        preview: "Preview"
+                    })
+                    .setValue(this.plugin.settings.paneToFocus)
+                    .onChange(async value => {
+                        this.plugin.settings.paneToFocus =
+                            value as PaneTypeSetting;
+                        await this.plugin.saveSettings();
+                    });
+            });
 
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+        new Setting(containerEl)
+            .setName("Link Panes")
+            .setDesc(
+                "Link the panes so that their scroll position and open file stay synced."
+            )
+            .addToggle(toggle => {
+                toggle
+                    .setValue(this.plugin.settings.linkPanes)
+                    .onChange(async value => {
+                        this.plugin.settings.linkPanes = value;
+                        await this.plugin.saveSettings();
+                    });
+            });
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+function getRootContainerSize(app: App) {
+    const rootContainer: HTMLElement = (app.workspace.rootSplit as any)
+        .containerEl;
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    if (rootContainer) {
+        return {
+            width: rootContainer.clientWidth,
+            height: rootContainer.clientHeight
+        };
+    } else {
+        console.warn(
+            `auto-split couldn't get root container, using window size`
+        );
+        return {
+            width: window.innerWidth,
+            height: window.innerHeight
+        };
+    }
 }
